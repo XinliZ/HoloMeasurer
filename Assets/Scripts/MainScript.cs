9e7ecd011c77204276423af60e77d29677f8327d
XinliZ <xlzou@live.com>
2016-05-21 17:36:58 -0700

    Initial commit for the project

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+
+public class MainScript : MonoBehaviour
+{
+
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+    MeasurerScript measurer;
+    GameObject cursor;
+
+    // Use this for initialization
+    void Start()
+    {
+        measurer = transform.Find("Pointer").gameObject.GetComponent<MeasurerScript>();
+        cursor = transform.Find("Cursor").gameObject;
+
+        keywords.Add("Measure", () =>
+        {
+            // Start measuring
+            // Pick the plane
+            // Set the measure pointer
+            var headPosition = Camera.main.transform.position;
+            var gazeDirection = Camera.main.transform.forward;
+
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+            {
+                // TODO: If the normal doesn't work, use the game object
+                measurer.StartMeasuring(hitInfo.point, hitInfo.normal, headPosition);
+            }
+            GetComponent<AudioSource>().Play();
+        });
+
+        keywords.Add("Stop", () =>
+        {
+            // Turn off the measure
+            measurer.StopMeasuring();
+            GetComponent<AudioSource>().Play();
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        
+        // If the raycast hit a hologram...
+        // Display the cursor mesh.
+        if (measurer.IsMeasuring())
+        {
+            cursor.transform.position = measurer.UpdatePoints(headPosition, gazeDirection);
+        }
+        else
+        {
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+            {
+                cursor.transform.position = hitInfo.point;
+                this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            }
+        }
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/MeasurerScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MeasurerScript : MonoBehaviour {
+    Vector3 startPoint;
+    Vector3 normal;
+    bool isMeasuring;
+
+    TextMesh textMesh;
+    Plane hitPlane;
+
+	// Use this for initialization
+	void Start () {
+        isMeasuring = false;
+        textMesh = transform.Find("Label").gameObject.GetComponent<TextMesh>();
+        hitPlane = transform.Find("HitPlane").gameObject.GetComponent<Plane>();
+	}
+
+    public void StartMeasuring(Vector3 startPoint, Vector3 normal, Vector3 headPosition)
+    {
+        this.startPoint = startPoint;
+        this.normal = normal;
+        this.isMeasuring = true;
+
+        var lookAtPoint = GetLookAtPoint(startPoint, normal, headPosition);
+
+        this.transform.position = startPoint;
+        this.transform.LookAt(lookAtPoint);
+        this.transform.Rotate(0, 180, 0);
+        //this.transform.Rotate(normal);
+        // Show the arrow
+    }
+
+    private Vector3 GetLookAtPoint(Vector3 startPoint, Vector3 normal, Vector3 headPosition)
+    {
+        Ray ray = new Ray(headPosition, -normal);
+        Plane p = new Plane(normal, startPoint);
+        float distance;
+        p.Raycast(ray, out distance);
+        return ray.GetPoint(distance);
+    }
+
+    public void StopMeasuring()
+    {
+        isMeasuring = false;
+        // Hide the arrow
+    }
+
+    public bool IsMeasuring()
+    {
+        return this.isMeasuring;
+    }
+
+    public Vector3 UpdatePoints(Vector3 headPosition, Vector3 gazeDirection)
+    {
+        var rawPoint = GetRayToPlanePoint(headPosition, gazeDirection, hitPlane);
+        var point = this.transform.TransformPoint(rawPoint);
+
+        float distance = 3;    // point.y - startPoint.y;
+        var scale = this.transform.localScale;
+        scale.y = distance;
+        this.transform.localScale = scale;
+        textMesh.text = distance.ToString();
+        return rawPoint;
+    }
+
+    public Vector3 GetRayToPlanePoint(Vector3 headPosition, Vector3 gazeDirection, Plane hitPlane)
+    {
+        float distanceToHitPlane;
+        var ray = new Ray(headPosition, gazeDirection);
+        hitPlane.Raycast(ray, out distanceToHitPlane);
+        return ray.GetPoint(distanceToHitPlane);
+    }
+
+}
